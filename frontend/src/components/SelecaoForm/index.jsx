11670215eda660e668304d205afbea42714bdb68
Manoel Junior <manuelitojunior@gmail.com>
2022-11-06 23:03:57 -0300

    Contador do Subtotal

--- frontend/src/components/SelecaoForm/index.jsx
+++ frontend/src/components/SelecaoForm/index.jsx
@@ -5,22 +5,27 @@ import { SelecaoCard } from '../SelecaoCard'
 
 export function SelecaoForm() {
   const [pedido_id, setPedido_id] = useState('')
-  const [pizza_id, setPizza_id] = useState('')
-  const [quantidade, setQuantidade] = useState('')
-  const [subtotal, setSubtotal] = useState('')
+  const [quantidade, setQuantidade] = useState(1)
   const [selecao, setSelecao] = useState([])
   let navigate = useNavigate()
+
+  //pizzas
+  const [pizzas, setPizzas] = useState([])
+  const [pizzaSelecionada, setPizzaSelecionada] = useState('')
+  const [preco, setPreco] = useState('')
 
   const url = 'http://localhost:3000/selecao'
 
   async function adicionarSelecao() {
     try {
-      await axios.post(url, { pedido_id, pizza_id, quantidade, subtotal })
+      //NÃO QUERO ENVIAR O SUBTOTAL, QUE SERÁ CALCULADO NO BACKEND. ELE APENAS APARECERÁ NA TELA PARA FINS DE CIÊNCIA AO USUÁRIO.
+      const pizza_id = pizzaSelecionada
+      await axios.post(url, { pedido_id, pizza_id, quantidade })
       getData()
       setPedido_id('')
-      setPizza_id('')
       setQuantidade('')
-      setSubtotal('')
+      setPizzaSelecionada('')
+      setPreco('')
     } catch (error) {
       console.error(error.message)
     }
@@ -41,11 +46,23 @@ export function SelecaoForm() {
 
   async function getData() {
     try {
+      //get Selecao
       const resultado = await axios.get(url)
       setSelecao(resultado.data)
+
+      //get Pizzas
+      const res_pizzas = await axios.get('http://localhost:3000/pizzas')
+      setPizzas(res_pizzas.data)
     } catch (error) {
       console.error(error.message)
     }
+    //get Subtotal
+  }
+
+  function getSubtotal() {
+    const pizzaFilter = pizzas.filter(pizza => pizza.id == pizzaSelecionada)
+    const precoFilter = pizzaFilter[0].preco
+    setPreco(precoFilter)
   }
 
   useEffect(() => {
@@ -66,34 +83,36 @@ export function SelecaoForm() {
           />
         </div>
         <div className="form-group">
-          <input
-            type="text"
+          <select
             className="form-control-md"
-            placeholder="Digite o ID. da Pizza"
-            id="pizza_id"
-            value={pizza_id}
-            onChange={e => setPizza_id(e.target.value)}
-          />
+            value={pizzaSelecionada}
+            onChange={e => setPizzaSelecionada(e.target.value)}
+            onClick={() => getSubtotal()}
+          >
+            {pizzas.map(pizza => {
+              return (
+                <option key={pizza.id} value={pizza.id}>
+                  {pizza.sabor} +R$ {pizza.preco}
+                </option>
+              )
+            })}
+          </select>
         </div>
         <div className="form-group">
           <input
-            type="text"
+            type="number"
             className="form-control-md"
             placeholder="Digite a Quantidade"
             id="quantidade"
             value={quantidade}
-            onChange={e => setQuantidade(e.target.value)}
+            onChange={e => {
+              setQuantidade(e.target.value)
+              getSubtotal()
+            }}
           />
         </div>
         <div className="form-group">
-          <input
-            type="text"
-            className="form-control-md"
-            placeholder="Digite o Subtotal"
-            id="subtotal"
-            value={subtotal}
-            onChange={e => setSubtotal(e.target.value)}
-          />
+          <label htmlFor="text">SUBTOTAL: {preco * quantidade}</label>
         </div>
         <button
           type="button"
